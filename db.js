e018fb3766b417297cde54fc868a986b94943be6
Rishi <rishiekandala@gmail.com>
2024-04-25 22:47:02 +0530

    server setup done

--- db.js
+++ db.js
@@ -21,18 +21,4 @@ const userSchema = new mongoose.Schema({
         trim: true,
         maxLength: 50
     },
-    lastName: {
-        type: String,
-        required: true,
-        trim: true,
-        maxLength: 50
-    }
-    
-});
-
-const User = mongoose.model('User', userSchema);
-
-module.exports = {
-    User
-};
-
+    lastN
--- index.js
+++ index.js
@@ -3,14 +3,27 @@ const cors = require('cors');
 const rootRouter = require("./routes/index");
 const userRouter = require("./routes/user")
 
-router.use(cors());
+const app = express();
+
 const router = express.Router();
+router.use(cors());
+router.use(express.json());
 router.use("/api/v1", rootRouter);
-router.use("/user", userRouter)
+router.use("/user", userRouter);
+
+app.listen(3000);
+
+console.log("server running")
+app.get("/",function(req,res){
+    res.json("Hello it is working")
+})
+
+
+
+
 
 
 module.exports = router;
-
 
 
 

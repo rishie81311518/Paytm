82479a6f230ce9a2abd45c634e4dce633b66c09f
Rishi <rishiekandala@gmail.com>
2024-05-04 12:04:42 +0530

    endpoint for users to transfer money

--- routes/account.js
+++ routes/account.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const { authMiddleware } = require("../middleware");
 const { Account } = require("../db");
 const router = express.Router();
@@ -13,6 +14,46 @@ router.get("/balance", authMiddleware, async (req, res) => {
   });
 });
 
+router.post("/transfer", authMiddleware, async (req, res) => {
+  const session = await mongoose.startSession();
 
+  session.startTransaction();
+  const { amount, to } = req.body;
+
+  const account = await Account.findOne({ userId: req.userId }).session(
+    session
+  );
+
+  if (!account || account.balance < amount) {
+    await session.abortTransaction();
+    return res.status(400).json({
+      message: "Insufficient balance",
+    });
+  }
+
+  const toAccount = await Account.findOne({ userId: to }).session(session);
+
+  if (!toAccount) {
+    await session.abortTransaction();
+    return res.status(400).json({
+      message: "Invalid account",
+    });
+  }
+
+  await Account.updateOne(
+    { userId: req.userId },
+    { $inc: { balance: -amount } }
+  ).session(session);
+  await Account.updateOne(
+    { userId: to },
+    { $inc: { balance: amount } }
+  ).session(session);
+
+  await session.commitTransaction();
+
+  res.json({
+    message: "Transfer successful",
+  });
+});
 
 module.exports = router;

4f23dea7678e047fda98c7d28cf5261f139dca60
Rishi <rishiekandala@gmail.com>
2024-05-04 22:45:32 +0530

    added all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Dashboard } from "./pages/Dashboard";
+import { SendMoney } from "./pages/SendMoney";
+import { Signin } from "./pages/Signin";
+import { Signup } from "./pages/Signup";
+
 function App() {
   return (
-   <BrowserRouter>
-   <Routes>
-   <Route path="/signup" element={<Signup />} />
-   <Route path="/signin" element={<Signin/>}  />
-   <Route path="/dashboard" element={<Dashboard />} />
-   <Route path="/send" element={<SendMoney />} />
-   </Routes>
-   </BrowserRouter>
-
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/send" element={<SendMoney />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
